1e2c3e48f12cf0e18f3d7784dafabc48b3954f8e
Antonio Saborido <antonio.saborido01@gmail.com>
2024-03-15 19:46:59 +0100

    tp tester schema

---
+++ src/app/pages/metrics-tester/metrics-tester.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metrics-tester',
+  templateUrl: './metrics-tester.component.html',
+  styleUrls: ['./metrics-tester.component.css']
+})
+export class MetricsTesterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/tpa-loaded/tpa-loaded.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tpa-loaded',
+  templateUrl: './tpa-loaded.component.html',
+  styleUrls: ['./tpa-loaded.component.css']
+})
+export class TpaLoadedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
