e16050411c1b98aead4300f9978a556aaede8966
Antonio Saborido <antonio.saborido01@gmail.com>
2024-03-17 01:06:40 +0100

    view / edit / delete component

--- src/app/pages/tpa-management/tpa-view/tpa-view.component.ts
+++ src/app/pages/tpa-management/tpa-view/tpa-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-tpa-view',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tpa-view.component.css']
 })
 export class TpaViewComponent implements OnInit {
+  tpaId!: string;
+  tpaData: any;
 
-  constructor() { }
+  metrics: any = {};
+  guarantees: any[] = [];
+  constructor(private http: HttpClient, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.tpaId = id;
+        this.http.get(`http://localhost:5400/api/v6/agreements/${id}`).subscribe(data => {
+          this.tpaData = data;
+          if (this.tpaData && this.tpaData.terms && this.tpaData.terms.metrics) {
+            this.metrics = this.tpaData.terms.metrics;
+          } else {
+            console.error('Cannot read, invalid data');
+          }
+          if (this.guarantees && this.tpaData.terms && this.tpaData.terms.guarantees) {
+            this.guarantees = this.tpaData.terms.guarantees;
+          } else {
+            console.error('Cannot read, invalid data');
+          }
+        });
+      }
+    });
   }
 
+  goBack() {
+    this.location.back();
+  }
 }
