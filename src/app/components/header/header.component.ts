4c3f50ad3b307eb2921aedb8274ce069b0ce8637
Antonio Saborido <antonio.saborido01@gmail.com>
2024-03-21 00:08:50 +0100

    traducciones totales

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private translate: TranslateService) {}
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
   ngOnInit(): void {
   }
 
