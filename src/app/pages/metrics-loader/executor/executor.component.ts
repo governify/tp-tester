9827348a71316f468303857dfd680d795e7b9ac9
Antonio Saborido <antonio.saborido01@gmail.com>
2024-04-13 13:12:21 +0200

    bug fix
    date update

--- src/app/pages/metrics-loader/executor/executor.component.ts
+++ src/app/pages/metrics-loader/executor/executor.component.ts
@@ -226,6 +226,28 @@ export class ExecutorComponent implements OnInit {
     this.window.from = startOfHour.toISOString();
     this.window.initial = startOfHour.toISOString();
     this.window.end = endOfHour.toISOString();
+
+    const data = JSON.parse(this.data);
+    if (data.metric.window) {
+      data.metric.window.type = this.window.type;
+      data.metric.window.period = this.window.period;
+      data.metric.window.initial = this.window.initial;
+      data.metric.window.from = this.window.from;
+      data.metric.window.end = this.window.end;
+      data.metric.window.timeZone = this.window.timeZone;
+    }
+    this.glassmatrixService.updateFile(this.fileName, data).subscribe(
+      () => {
+        this.message = 'File saved successfully';
+        this.messageClass = 'success';
+      },
+      (error) => {
+        this.message = 'An error occurred: ' + error;
+        this.messageClass = 'error';
+      }
+    );
+    // Recargar la página
+    location.reload();
   }
 
   goBack(): void {
